2701cc4acaec060deac5c72288bda348d0bd0224
Renato Nakamura <65171479+Renato-Nakamura@users.noreply.github.com>
2022-07-12 13:54:54 -0300

    :bug: (controller): bug already exists

--- src/controllers/lists.controller.js
+++ src/controllers/lists.controller.js
@@ -17,7 +17,8 @@ export const getListController = async (req, res) => {
 export const addListController = async (req, res) => {
   const listBody = req.body;
   const list = await getListService(listBody.listTitle);
-  if(list) return res.status(400).send('list already exists')
+  console.log(list,'rere')
+  if(list.length) return res.status(400).send('list already exists')
   const newList = await addListService(listBody);
 
   res.status(201).send(newList);
